cc0dd6cc34da802a2f7e84ae9da708f2704992a9
Maciej Tokarz <maciej.tokarz1@wp.pl>
2021-03-09 13:10:41 +0100

    test for get zadanie1 z lab03

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    try {
+        res.send("Hello from my node app!")
+    }
+    catch (error) {
+        res.send('error' + error)
+    }
+})
+
+app.listen(8080)
